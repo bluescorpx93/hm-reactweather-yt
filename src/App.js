a8c3964de03fa9f209978d2c0d020df902e12779
Toufiq <burningraven06@gmail.com>
2018-02-22 17:55:13 +0600

    create first react component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <p>Hey</p> 
+        <h1> Hello! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
