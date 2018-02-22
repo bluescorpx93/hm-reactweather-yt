d4a0e320f7b120e0e82f07f0ee2e730032fe58ed
Toufiq <burningraven06@gmail.com>
2018-02-22 18:02:52 +0600

    create new component

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Title extends React.Component{
+	render(){
+		return(
+			<div> 
+				<div> 
+					<h1> Weather Finder </h1>
+					<p> Find out weather conditions </p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Title;
