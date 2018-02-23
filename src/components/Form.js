3876338c6286761540d6073e1df4ce6140eef142
Toufiq <burningraven06@gmail.com>
2018-02-23 13:46:10 +0600

    use function from another component

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ class Form extends React.Component{
 	render(){
 		return(
 			<div>
-				<form>
+				<form onSubmit={this.props.getWeatherFunc}>
 					<input type='text' name='city' placeholder='City' />
 					<input type='text' name='country' placeholder='Country' />
 					<button type='submit'> Search </button>
