06bf9b3ddbeba22d1f3915d72c83837524d15970
Toufiq <burningraven06@gmail.com>
2018-02-23 14:06:34 +0600

    pass states to component

--- src/App.js
+++ src/App.js
@@ -8,20 +8,39 @@ const API_KEY = "def96d2721e4de07220724e98674cc45";
 class App extends React.Component{
 
   getWeather = async (e) => {
+    state = {
+      tempareture: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: undefined
+    }
     e.preventDefault();
+    
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
     const data = await api_call.json();
+
     console.log(data);
+
+    this.setState({
+      tempareture: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   }
   render(){
     return(
       <div>
         <Titles/>
         <Form getWeatherFunc={this.getWeather} />
-        <Weather />
+        <Weather temperature={this.state.temperature} city={this.state.city} country={this.state.country} humidity={this.state.humidity} description={this.state.description} error={this.state.error} />
       </div>
     );
   }
