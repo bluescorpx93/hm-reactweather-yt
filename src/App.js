a0c98df8d49acc8eb65b1f03ae2da0284ee2e5e0
Toufiq <burningraven06@gmail.com>
2018-02-23 13:37:01 +0600

    write function for api call

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "def96d2721e4de07220724e98674cc45";
+
 class App extends React.Component{
+
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`)
+    const data = await api_call.json();
+    console.log(data);
+  }
   render(){
     return(
       <div>
