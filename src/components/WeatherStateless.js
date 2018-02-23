17fd4c05ee15e353056026581c4217f9af0ee1fa
Toufiq <burningraven06@gmail.com>
2018-02-23 14:51:54 +0600

    remove braces

--- src/components/WeatherStateless.js
+++ src/components/WeatherStateless.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
-const WeatherStateless = (props) => {
-	return (
-		<div>
-			{ props.city && props.country && <p> <strong>Location:</strong> {props.city}, {props.country} </p> } 
+const WeatherStateless = (props) => (
+	<div>
+		{ props.city && props.country && <p> <strong>Location:</strong> {props.city}, {props.country} </p> } 
 
-			{ props.temperature && <p> <strong>Temperature</strong>: {props.temperature} </p> } 
+		{ props.temperature && <p> <strong>Temperature</strong>: {props.temperature} </p> } 
 
-			{ props.humidity && <p> <strong>Humidity</strong>: {props.humidity} </p> } 
+		{ props.humidity && <p> <strong>Humidity</strong>: {props.humidity} </p> } 
 
-			{ props.description && <p> <strong>Conditions</strong>: {props.description} </p> } 
+		{ props.description && <p> <strong>Conditions</strong>: {props.description} </p> } 
 
-			{ props.error && <p> <strong>Error</strong>: {props.error} </p> } 
+		{ props.error && <p> <strong>Error</strong>: {props.error} </p> } 
 		</div>
-
-		);
-}
+	);
 export default WeatherStateless;
